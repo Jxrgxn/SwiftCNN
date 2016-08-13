b2615ae15d8c8d79a571ffcba5ac02770424412e
Jxrgxn <basel.farag@gmail.com>
2016-08-13 03:14:29 -0500

    init

---
+++ Contents.swift
@@ -0,0 +1,7 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+/*Convolutional Neural Networks are commonly used in computer vishion. CNNs take advantage of a signal processing known as convolution. Convolution is weighted su of collection of neighboring elements. 
+ usually this is an array or matrix.*/
